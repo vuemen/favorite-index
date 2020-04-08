675f83345bb1be8b2c0969761ac4dac5a957019b
SHUN <869637266@qq.com>
2020-04-08 22:17:16 +0800

    工程初始化

---
+++ src/main/java/com/shun/favoriteindex/FavoriteIndexApplication.java
@@ -0,0 +1,13 @@
+package com.shun.favoriteindex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoriteIndexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FavoriteIndexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shun/favoriteindex/FavoriteIndexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shun.favoriteindex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoriteIndexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
