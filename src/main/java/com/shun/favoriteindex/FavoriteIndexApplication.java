f89b1b3f17e7f8134ec988db56f9e61d89cb2843
SHUN <869637266@qq.com>
2020-04-13 22:08:09 +0800

    工程代码初始化

--- src/main/java/com/shun/favoriteindex/FavoriteIndexApplication.java
+++ src/main/java/com/shun/favoriteindex/FavoriteIndexApplication.java
@@ -3,6 +3,7 @@ package com.shun.favoriteindex;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//主程序类
 @SpringBootApplication
 public class FavoriteIndexApplication {
 
--- src/test/java/com/shun/favoriteindex/FavoriteIndexApplicationTests.java
+++ src/test/java/com/shun/favoriteindex/FavoriteIndexApplicationTests.java
@@ -1,13 +1,27 @@
 package com.shun.favoriteindex;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
 
 @SpringBootTest
 class FavoriteIndexApplicationTests {
 
+    @Autowired
+    DataSource dataSource;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void dataSourceTest() {
+        System.out.println("=========================");
+        System.out.println(dataSource);
+        System.out.println(dataSource.getClass().getName());
+        System.out.println("=========================");
     }
 
 }
